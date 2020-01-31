fbc61ccc17c496b3f4fc399de4bacf897f485b65
Albert Wen <albertwen2000@gmail.com>
2020-01-31 18:02:36 -0500

    coded basic IO, users enter in assumptions and expression to check

---
+++ propesize.hs
@@ -0,0 +1,29 @@
+import PropParse
+import PropCalc
+import Control.Monad
+import Text.Parsec
+
+main = do
+    putStrLn "How many statements would you like to assume?"
+    num <- getLine
+    putStrLn ("Reading in " ++ num ++ ".")
+    assumptions <- sequence (replicate (read num::Int) getLine)
+    putStrLn ("What statement would you like to check?")
+    toCheck <- getLine
+    putStrLn (proofRes assumptions toCheck)
+
+rExp :: String -> Exp
+rExp e = case (parse pExp "" e) of
+           Left err -> error (show err)
+           Right exp -> exp
+
+--proofRes' :: Maybe Bool -> String
+--pproofRes' (Just True) = "Provable True"
+--pproofRes' (Just False) = "Provable False"
+--pproofRes' Nothing = "Indeterminate under given assumptions"
+
+proofRes :: [String] -> String -> String
+proofRes givenS proveS = proofRes' (provableGiven (map rExp givenS) (rExp proveS))
+                    where
+                        proofRes' True = "Provable true."
+                        proofRes' False = "Not provable."
